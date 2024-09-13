c2be2bd8b096c3cf506bf889d586ff4aaf5d0a8f
Fuwn <contact@fuwn.me>
2024-09-13 06:22:58 -0700

    chore(flake): add git hooks and checks

--- flake.nix
+++ flake.nix
@@ -17,8 +17,10 @@
 
   outputs =
     {
+      self,
       nixpkgs,
       flake-utils,
+      pre-commit-hooks,
       ...
     }:
     flake-utils.lib.eachDefaultSystem (system: {
@@ -152,5 +154,22 @@
         };
 
       formatter = nixpkgs.legacyPackages."${system}".nixfmt-rfc-style;
+
+      checks.pre-commit-check = pre-commit-hooks.lib.${system}.run {
+        src = ./.;
+
+        hooks = {
+          deadnix.enable = true;
+          flake-checker.enable = true;
+          nixfmt-rfc-style.enable = true;
+          statix.enable = true;
+        };
+      };
+
+      devShells.default = nixpkgs.legacyPackages.${system}.mkShell {
+        inherit (self.checks.${system}.pre-commit-check) shellHook;
+
+        buildInputs = self.checks.${system}.pre-commit-check.enabledPackages;
+      };
     });
 }
