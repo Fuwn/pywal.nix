944bcd8ecae4513c2e2e4c467973e1ca22384545
Fuwn <contact@fuwn.net>
2026-06-13 12:38:46 +0000

    fix(wrap): Import only the selected backend

--- wrap.py
+++ wrap.py
@@ -1,22 +1,8 @@
+import importlib
 import sys
 
-from pywal.backends import (
-    colorthief,
-    colorz,
-    fast_colorthief,
-    haishoku,
-    schemer2,
-    wal,
-)
-
 if __name__ == "__main__" and len(sys.argv) > 1:
-    print(
-        {
-            "colorthief": colorthief.get,
-            "colorz": colorz.get,
-            "fast_colorthief": fast_colorthief.get,
-            "haishoku": haishoku.get,
-            "schemer2": schemer2.get,
-            "wal": wal.get,
-        }[sys.argv[1]](sys.argv[2], sys.argv[3] == "1" if len(sys.argv) > 3 else False)
-    )
+    backend = importlib.import_module(f"pywal.backends.{sys.argv[1]}")
+    light = sys.argv[3] == "1" if len(sys.argv) > 3 else False
+
+    print(backend.get(sys.argv[2], light))
